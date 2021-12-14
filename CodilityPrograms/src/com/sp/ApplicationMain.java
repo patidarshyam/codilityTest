ff5cea1f42f6fd5cc90393989224aa0a235f3952
Shyam <patidarshyam@github.com>
2021-12-14 10:49:18 +0530

    Codility Project

---
+++ CodilityPrograms/src/com/sp/ApplicationMain.java
@@ -0,0 +1,9 @@
+package com.sp;
+
+public class ApplicationMain {
+
+	public static void main(String [] args) {
+		
+		System.out.println("Test");
+	}
+}
---
+++ CodilityPrograms/src/com/sp/arrays/OddOccurencesInArray.java
@@ -0,0 +1,5 @@
+package com.sp.arrays;
+
+public class OddOccurencesInArray {
+
+}
